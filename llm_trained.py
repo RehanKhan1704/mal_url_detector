778ef5d1973c0e1fa73a9c50aba68af08bed3168
RehanKhan1704 <rehanmkhan17@gmail.com>
2025-10-13 15:04:58 +0530

    Add files via upload

---
+++ llm_trained.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+
+# Load the model and tokenizer
+#model_name = "r3ddkahili/final-complete-malicious-url-model"
+tokenizer = AutoTokenizer.from_pretrained("distilbert_url")
+model = AutoModelForSequenceClassification.from_pretrained("distilbert_url")
+
+# Example URL
+url = "merge.dev/blog/gemini-api-key"
+
+# Tokenize and predict
+inputs = tokenizer(url, return_tensors="pt", truncation=True, padding=True, max_length=128)
+with torch.no_grad():
+    outputs = model(**inputs)
+    prediction = torch.argmax(outputs.logits).item()
+
+# Mapping prediction to labels
+label_map = {0: "Benign", 1: "Defacement", 2: "Phishing", 3: "Malware"}
+print(f"Prediction: {label_map[prediction]}")
